5f36df2a8c7f9e6c5120a6d8ac1eebeeff196c5e
Nabillera <vatcharadzenini@gmail.com>
2023-11-27 21:09:26 +0400

    updated the form

--- src/App.js
+++ src/App.js
@@ -1,48 +1,97 @@
 import React, { useState } from 'react'
 import { Movie } from './Movie'
 import "./App.css"
+import img from "./Shrek.webp"
 
 const App = () => {
-  let [movies, setMovies] = useState(
-      [{
-        name: "Shrek",
-        director: "Andrew Adamson",
-        release: "2001",
-        genre: "Comedy",
-        poster: "https://en.wikipedia.org/wiki/Shrek#/media/File:Shrek_(2001_animated_feature_film).jpg"
-      },
-      {
-        name: "Shrek 2",
-        director: "Andrew Adamson",
-        release: "2004",
-        genre: "Fantasy",
-        poster: "https://en.wikipedia.org/wiki/Shrek_2#/media/File:Shrek_2_poster.jpg"
-      },
-      {
-        name: "Shrek the Third",
-        director: "Chris Miller",
-        release: "2007",
-        genre: "Fantasy",
-        poster: "https://en.wikipedia.org/wiki/Shrek_the_Third#/media/File:Shrek_the_Third_(2007_animated_feature_film).jpg"
-      },
-      {
-        name: "Shrek Forever After",
-        director: "Mike Mitchell",
-        release: "2010",
-        genre: "Action",
-        poster: "https://en.wikipedia.org/wiki/Shrek_Forever_After#/media/File:Shrek_Forever_After_(2010_animated_feature_film).jpg"
-      }]
-  )
-   
-  return (
 
-    <div>
-      <div>
-      {movies.map((movie) => {
+  const [movie, setMovie] = useState({
+    name: "",
+    director: "",
+    release: "",
+    genre: "",
+    poster: ""
+  });
+
+  const [movies, setMovies] = useState(
+    [{
+      name: "Shrek",
+      director: "Andrew Adamson",
+      release: "2001",
+      genre: "Comedy",
+      poster: "https://upload.wikimedia.org/wikipedia/en/7/7b/Shrek_%282001_animated_feature_film%29.jpg"
+    },
+    {
+      name: "Shrek 2",
+      director: "Andrew Adamson",
+      release: "2004",
+      genre: "Fantasy",
+      poster: "https://upload.wikimedia.org/wikipedia/en/b/b9/Shrek_2_poster.jpg"
+    },
+    {
+      name: "Shrek the Third",
+      director: "Chris Miller",
+      release: "2007",
+      genre: "Fantasy",
+      poster: "https://upload.wikimedia.org/wikipedia/en/2/22/Shrek_the_Third_%282007_animated_feature_film%29.jpg"
+    },
+    {
+      name: "Shrek the Halls",
+      director: "Gary Trousdale",
+      release: "2007",
+      genre: "Comedy",
+      poster: "https://upload.wikimedia.org/wikipedia/en/b/b7/Shrek_the_Halls_poster.jpg"
+    },
+    {
+      name: "Shrek Forever After",
+      director: "Mike Mitchell",
+      release: "2010",
+      genre: "Action",
+      poster: "https://upload.wikimedia.org/wikipedia/en/7/70/Shrek_Forever_After_%282010_animated_feature_film%29.jpg"
+    }]
+  )
+
+  const saveInput = (event) => {
+    const userInput = event.target.value;
+    setMovie({
+      ...movie,
+      [event.target.name]: userInput
+    })
+  };
+
+  const saveMovie = () => {
+    movies.push(movie);
+    setMovie({
+      ...movie,
+      name: "",
+      director: "",
+      release: "",
+      genre: "",
+      poster: ""
+    })
+  }
+  return (
+    <div id="main-container">
+      <div id="new-entry-container">
+        <img id="illustration-1" src={img} />
+        <div id="form">
+          <input onChange={saveInput} name="name" className="input-field" placeholder="Title" />
+          <input onChange={saveInput} name="release" className="input-field" placeholder="Release date" />
+          <input onChange={saveInput} name="genre" className="input-field" placeholder="Genre" />
+          <input onChange={saveInput} name="director" className="input-field" placeholder="Director" />
+          <input onChange={saveInput} name="poster" className="input-field" placeholder="Poster link" />
+          <button onClick={saveMovie}>Add Movie</button>
+        </div>
+        <img id="illustration-2" src={img} />
+      </div>
+
+      <div id="movie-list-container">
+        {movies.map((movie) => {
           return (
             <Movie
               name={movie.name}
               release={movie.release}
+              genre={movie.genre}
               director={movie.director}
               poster={movie.poster}
             />
--- src/Movie.js
+++ src/Movie.js
@@ -2,10 +2,11 @@ import "./Movie.css";
 
 export function Movie(props){
 return (<div id="movie-container">
-    <img src={props.poster}/>
+    <img id="poster" src={props.poster}/>
     <div>
         <p id="name">{props.name}</p>
         <p>{props.release}</p>
+        <p>{props.genre}</p>
         <p>{props.director}</p>
     </div>
 </div>)
